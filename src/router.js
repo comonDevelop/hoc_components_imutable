a1f4c216462282e7280d7c330fd3d4f9823b2102
ludan <1107743896@qq.com>
2019-09-09 17:29:57 +0800

    打印组件应用

--- src/router.js
+++ src/router.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import IndexPage from './routes/IndexPage';
 import Hoc from './routes/Hoc/index';
-import ReactHooks from './routes/ReactHooks/index'
+import ReactHooks from './routes/ReactHooks/index';
+import ReactPrint from './routes/Print';
+
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
@@ -10,6 +12,7 @@ function RouterConfig({ history }) {
         <Route path="/" exact component={IndexPage} />
         <Route path="/hoc" exact component={Hoc} />
         <Route path="/ReactHooks" exact component={ReactHooks} />
+        <Route path="/ReactPrint" exact component={ReactPrint} />
       </Switch>
     </Router>
   );
