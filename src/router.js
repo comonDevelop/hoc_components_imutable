20c02f98b2fbf83121e9996eaace37931e867195
GongXiaoZhu <1107743896@qq.com>
2019-11-06 09:42:09 +0800

    vido展示

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import IndexPage from './routes/IndexPage';
 import Hooks from './routes/Hooks';
+import Video from './routes/Video/index';
 
 function RouterConfig({ history }) {
   return (
@@ -9,6 +10,7 @@ function RouterConfig({ history }) {
       <Switch>
         <Route path="/" exact component={IndexPage} />
         <Route path="/hooks" exact component={Hooks} />
+        <Route path="/video" exact component={Video} />
       </Switch>
     </Router>
   );
