b993c85944c4f4134c36958ba80e9e730e66ed54
ld <ld>
2019-12-23 10:16:38 +0800

    hook 例子

--- src/routes/Hooks.js
+++ src/routes/Hooks.js
@@ -3,16 +3,33 @@ import React, {
     useEffect, 
     useReducer
 } from "react";
-import { Button } from 'antd'
+import { Input, Button } from 'antd'
 
 const Hooks = () => {
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
+    const [fruits, setFruits] = useState(['banana', 'Apple'])
+    const [fruit, setFruit] = useState("")
     return (
         <div style={{textAlign:'center', marginTop: '60px'}}>
             <p>这是count: {count}</p>
             <Button onClick={() => setCount((count) => {
                 return count + 1
             })}>点击修改count</Button>
+            <br/><br/>
+            <p>---------------------------------------------------------------</p>
+            <div style={{width: '400px', padding: "20px", margin: '0 auto'}}>
+                <Input value={fruit} onChange={(e) => setFruit(e.target.value)}/>
+                <Button type="primary" onClick={() => {
+                    console.log(fruits, '------fruits---');
+                    console.log(fruit, '-----item----');
+                    fruit != '' && setFruits([...fruits, fruit])
+                }}>添加水果</Button>
+                <div style={{background: '#03ccdd'}}>
+                    {fruits.map((k, i) => <p key={i}>{k}</p>)}
+                </div>
+            </div>
+            <p>---------------------------------------------------------------</p>
+            
         </div>
     )
 }
